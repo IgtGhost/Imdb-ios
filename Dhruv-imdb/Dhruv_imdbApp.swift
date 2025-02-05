c88e4ce675a193ed53c26fba7d85e84bf9e2ce3d
atef fawaz <atefawaz@gmail.com>
2025-02-05 16:43:15 +0100

    Initial Commit

---
+++ Dhruv-imdb/Dhruv_imdbApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dhruv_imdbApp.swift
+//  Dhruv-imdb
+//
+//  Created by atef fawaz on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Dhruv_imdbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dhruv-imdbUITests/Dhruv_imdbUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Dhruv_imdbUITestsLaunchTests.swift
+//  Dhruv-imdbUITests
+//
+//  Created by atef fawaz on 05/02/2025.
+//
+
+import XCTest
+
+final class Dhruv_imdbUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
